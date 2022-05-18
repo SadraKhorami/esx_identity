69b22f93b94127201189082ddf369ab699276fd4
Sadra Khorami <30285010+SadraKhorami@users.noreply.github.com>
2022-05-17 23:37:36 -0700

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_script 'identity.lua'
+
+ui_page('html/ui.html')
+
+files({
+  "html/*",
+  "html/assets/css/*",
+  "html/assets/fonts/*",
+  "html/assets/images/*",
+  "html/assets/js/*"
+
+})
+
+
+
