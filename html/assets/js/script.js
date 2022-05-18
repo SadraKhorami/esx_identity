fcabd6b9f276f33ea59773eddbe0c1812e785a8b
Sadra Khorami <30285010+SadraKhorami@users.noreply.github.com>
2022-05-18 00:18:53 -0700

    Add files via upload

---
+++ html/assets/js/script.js
@@ -0,0 +1,30 @@
+console.clear();
+
+var el = {};
+
+$('.placeholder').on('click', function (ev) {
+  $('.placeholder').css('opacity', '0');
+  $('.list__ul').toggle();
+});
+
+ $('.list__ul a').on('click', function (ev) {
+   ev.preventDefault();
+   var index = $(this).parent().index();
+   
+   $('.placeholder').text( $(this).text() ).css('opacity', '1');
+   
+   //console.log($('.list__ul').find('li').eq(index).html());
+   
+   $('.list__ul').find('li').eq(index).prependTo('.list__ul');
+   $('.list__ul').toggle();   
+   
+ });
+
+
+$('select').on('change', function (e) {
+  
+  $('.placeholder').text(this.value);
+  
+  $(this).animate({width: $('.placeholder').width() + 'px' });
+  
+});
